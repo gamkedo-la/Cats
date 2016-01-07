4cd2f179204922d43c10be40519895e30f143973
Jeremy Kenyon <lanthos@gmail.com>
2016-01-06 22:23:18 -0800

    Added random squeak to rat
    The rat will now squeak at random intervals.

---
+++ Assets/Scripts/MouseSqueak.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseSqueak : MonoBehaviour {
+
+	AudioSource squeak;
+	float squeakInterval = 4.0f;
+	// Use this for initialization
+	void Start () {
+		squeak = GetComponent<AudioSource> ();
+		StartCoroutine (Squeak ());
+	}
+
+	IEnumerator Squeak(){
+		yield return new WaitForSeconds (squeakInterval);
+		squeak.Play ();
+		squeakInterval = Random.Range (3.0f, 10.0f);
+		StartCoroutine (Squeak ());
+	}
+}

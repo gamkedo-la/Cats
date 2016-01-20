6a0b78cf20cc8e092146bf7944a79063285040e5
patricknugent <nugget84@gmail.com>
2016-01-20 10:36:21 -0800

    Updated Scoring System
    Rolling Score with sound effects.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,17 +7,40 @@ public class ScoreManager : MonoBehaviour
 
     public static int score;
     public Text text;
+    public AudioSource rollingScoreSound;
+
+    //the rolling score displayed on screen
+    public int rollingScore;
+
+    //the amount of points to add at a time to the rolling score
+    public int rollingScoreInterval = 10;
 
     void Start()
     {
         text = GetComponent<Text>();
+        rollingScoreSound = GetComponent<AudioSource>();
         score = 0;
+        rollingScore = 0;
     }
 
     void Update()
     {
+        
+        if(rollingScore < score)
+        {
+            rollingScore += rollingScoreInterval;
+            rollingScoreSound.Play();
 
-        text.text = "" + score;
+        }else if(rollingScore >= score)
+        {
+            rollingScore = score;
+        }
+        else
+        {
+            rollingScore = 0;
+        }
+
+        text.text = "" + rollingScore;
     }
 
     public static void AddPoints(int numberOfPoints)

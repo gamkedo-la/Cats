ed64cdbd41288caf1b188ff13bacf97eb2da6bc8
patricknugent <nugget84@gmail.com>
2016-01-24 23:22:31 -0800

    Added Timer

---
+++ Assets/Scripts/TimeDialManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeDialManager : MonoBehaviour {
+
+    public float startingTime;
+    
+    private Image radialImage;
+    private float currentTime;
+    private bool paused;
+
+    // Use this for initialization
+    void Start () {
+
+        paused = false;
+        radialImage = GetComponent<Image>();
+        currentTime = startingTime;
+            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!paused && currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            radialImage.fillAmount = currentTime / startingTime;
+
+        }
+
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = startingTime;
+    }
+
+    public void StartTimer()
+    {
+        paused = false;
+    }
+
+    public void StopTimer()
+    {
+        paused = true;
+    }
+
+    public bool CheckIfTimeIsUp()
+    {
+        if (currentTime <= 0)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+    public float startingTime;
+
+    private Text displayTimer;
+    private float currentTime;
+    private bool paused;
+
+    // Use this for initialization
+    void Start () {
+
+        displayTimer = GetComponent<Text>();
+        currentTime = startingTime;
+        displayTimer.text = currentTime.ToString();
+            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!paused && currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            displayTimer.text = Mathf.Round(currentTime).ToString();
+        }
+
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = startingTime;
+    }
+
+    public void StartTimer()
+    {
+        paused = false;
+    }
+
+    public void StopTimer()
+    {
+        paused = true;
+    }
+
+    public bool CheckIfTimeIsUp()
+    {
+        if (currentTime <= 0)
+            return true;
+        else
+            return false;
+    }
+}

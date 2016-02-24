63d87b72fa61ee883f835dde59a203ceb4056e4a
DesertJay <desertjaybirdie@gmail.com>
2016-02-24 00:11:06 -0800

    Cat Paw Cursor
    Added a PawCursor script and a temporary cat paw texture for the cursor.
    Created a CatPaw prefab and added it to the Main and Bedroom scenes.

---
+++ Assets/Scripts/PawCursor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PawCursor : MonoBehaviour {
+
+    public Texture2D pawTexture;
+
+    bool pawEnabled = false;
+    int ignoreLayers;
+
+    void Start() {
+       ignoreLayers = ~LayerMask.GetMask("Everything", "Nothing", "Default", "TransparentFX", "Ignore Raycast", "Water", "UI", "Click Tester", "Cat", "MouseClickOnly", "CamIgnore", 
+           "KittyLitter", "SphereCollider");
+    }
+
+    void Update() {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ignoreLayers)) {
+                this.PawEnable();
+            }
+            else {
+                this.PawDisable();
+            }
+    }
+
+    void PawEnable() {
+        Cursor.SetCursor(this.pawTexture, Vector2.zero, CursorMode.Auto);
+        this.pawEnabled = true;
+    }
+
+    void PawDisable() {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        this.pawEnabled = false;
+    }
+
+}

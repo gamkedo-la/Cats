fe7cb0697ba35fb30162494bb9ac267abf31cc72
Chris DeLeon <gamkedo@gmail.com>
2016-02-24 16:09:52 -0800

    People added from Arcade 88, stands added, moving near foreground in main scene now shows crowd and plays audience reaction. Spotlight added to track cat, lighting in main scene adjusted to keep audience more in dark

---
+++ Assets/PlaySoundWhenCatEnters.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundWhenCatEnters : MonoBehaviour {
+	public AudioClip[] soundArray;
+
+	void OnTriggerEnter(Collider whatEntered) {
+		if(whatEntered.name.Contains("Cat")) {
+			SoundManager.instance.PlayClipOn(soundArray[ Random.Range(0,soundArray.Length) ], Camera.main.transform.position, 1, Camera.main.transform);
+		}	
+	}
+}
---
+++ Assets/TrackCat.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackCat : MonoBehaviour {
+	public GameObject trackObj;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Slerp(transform.rotation,
+			Quaternion.LookRotation(trackObj.transform.position - transform.position), 4.0f * Time.deltaTime);
+	}
+}
